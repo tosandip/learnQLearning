33b32483fb13de1ecbc1dfb53d28c3eaced8ac25
sandipk <sandip.kumar2@mathworks.com>
2019-10-11 16:00:39 -0400

    first script

---
+++ qlearning-1.py
@@ -0,0 +1,23 @@
+import gym
+
+env = gym.make("MountainCar-v0")
+env.reset()
+
+# print some info about the env
+print(env.observation_space.high)
+print(env.observation_space.low)
+print(env.action_space.n)
+
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/ DISCRETE_OS_SIZE
+
+print(discrete_os_win_size)
+
+done = False
+
+while not done:
+    action = 2
+    new_state, reward, done, _ = env.step(action)
+    env.render()
+
+env.close()
